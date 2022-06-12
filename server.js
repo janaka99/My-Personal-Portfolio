11fc5024c31c8bf2014dfd1a6833480d20b2c68a
janaka <janakachamith882gmail.com>
2022-06-12 17:10:34 +0530

    First commit

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://github.com/janaka99/My-Personal-Portfolio",
     credentials: true,
   })
 );
