462292de25095b5cb4f01c3c035ac8184d039da8
janaka <janakachamith882gmail.com>
2022-06-13 14:51:33 +0530

    first commit

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(
     methods: ["GET", "PUT", "POST"],
   })
 );
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 const loginRoutes = require("./routes/Login");
 
@@ -216,14 +217,11 @@ app.get("/api/loadLanguage", async (req, res) => {
   );
 });
 
-//server static assets if we in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+});
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//server static assets if we in production
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
