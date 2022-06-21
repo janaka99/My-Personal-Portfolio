c9b158fa6ba0bef1c9e52980c6787ab800e4a801
janaka <janakachamith882gmail.com>
2022-06-21 13:55:53 +0530

    design commit

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     // origin: "http://localhost:3000",
-    origin: process.env.ORIGIN_URL,
+    origin: "https://janakachamith.herokuapp.com",
     credentials: true,
     methods: ["GET", "PUT", "POST"],
   })
